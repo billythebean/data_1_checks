dc3f0e92722e5848d00b351ba6be4d69ad93ecde
billythebean <95658551+billythebean@users.noreply.github.com>
2022-05-23 21:58:45 -0400

    Add files via upload

--- data_1_checks.py
+++ data_1_checks.py
@@ -1,25 +1,25 @@
 #first data check
 print("Hello! This is my first data check with Code Louisville.\n\t Proceed with caution!")
-question= input("Are you positive you want to proceed? ").lower()
-#import json module 
-import json
-import string
-#create a dictionary to a string
-str1= "string.ascii_letters + string.digits + !@#$%^&*()"
-d = dict.fromkeys(string.ascii_lowercase, 0)
-dict = { 'A'}
-if question == "yes":
-     print("Hello World!\n\t Please create a special password using these letters.") 
-     print(str1)
-else:
-     print("Goodbye World!")
+question= input("Are you positive you want to proceed? (YES OR NO) ").lower()
+name = input("What is your name? (TYPE YOUR FIRST NAME)" )
+#define dictionairies
+#create a dictionary to a string & added tuples
+password_requirements = [
+    {
+    "length" : 5,
+    "digits" : 9,
+    "characters" : "Hello World!"},
+    {
+    "length" : 6,
+    "digits" : 9,
+    "characters" : "Hello World!"}
+
+    ]
+password_list = ["characters",  "digits", "length"]
+numbers = ("Welcome! You will use these instructions to formulate your own temporary password when you log in next time! Make sure you choose a secure password next time. (Press enter)!")
+print(numbers)
+print(password_requirements[1]["length"], password_list)
+print(password_requirements[0]["characters"])
 
 
-
-
-
-
-   
-
-
-      
+ 
